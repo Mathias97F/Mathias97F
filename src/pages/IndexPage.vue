90342642762df112cafe54a9fc283c76e8bb6341
Mathias97F <128819703+Mathias97F@users.noreply.github.com>
2025-08-19 09:49:02 +0200

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="row items-center justify-center">
+    <q-card
+      class="q-pa-lg q-mx-auto q-my-xl text-center"
+      style="max-width: 500px;"
+    >
+      <q-card-section class="column items-center q-gutter-md">
+        <ExampleComponent
+          title="Hello World"
+          subtitle="Cavatech Tutorial"
+          subsubtitle="tutorial"
+          active
+        />
+        <ButtonComponent
+          buttontext="Click here"
+          link="https://www.combitech.se/"
+        />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import ExampleComponent from 'components/ExampleComponent.vue';
+import ButtonComponent from 'components/ButtonComponent.vue';
+</script>
